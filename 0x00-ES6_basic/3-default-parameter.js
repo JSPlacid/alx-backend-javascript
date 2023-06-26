235a63fff002e644ea7480e147cd1455c343a820
JSPlacid <ojoolusegun17@gmail.com>
2023-06-26 13:49:13 -0700

    default param

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,10 +1,4 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  if (expansion1989 = 34) {
-    expansion1989 = 89;
-  }
-
-  if (expansion2019 = 34) {
-    expansion2019 = 19;
-  }
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
+
