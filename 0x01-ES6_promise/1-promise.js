05da845bc6a0d73d2d4d84edf147891d18b99b82
JSPlacid <ojoolusegun17@gmail.com>
2023-06-27 09:00:14 -0700

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,17 +1,18 @@
-export function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-    // async API call
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    // Async API call or any other async operation
+    // using a setTimeout to simulate an asynchronous operation
     setTimeout(() => {
-	if (success) {
-	    const response = {
-		status: 200,
-		body: 'Success'
-	    };
-	    resolve(response); // Resolve the Promise with the response object
-	} else {
-	    const error = new Error('The fake API is not working currently');
-	    reject(error); // Reject the Promise with an error object
-	}
+      if (success) {
+        const response = {
+          status: 200,
+          body: 'Success'
+        };
+        resolve(response); // Resolve the Promise with the response object
+      } else {
+        const error = new Error('The fake API is not working currently');
+        reject(error); // Reject the Promise with an error object
+      }
     }, 2000); // Delay of 2000 milliseconds (2 seconds)
-    });
+  });
 }
