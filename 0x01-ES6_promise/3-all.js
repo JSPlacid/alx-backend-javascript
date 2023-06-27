b73b346b3195824ee86965a24ee647b305f98fb6
JSPlacid <ojoolusegun17@gmail.com>
2023-06-27 09:36:49 -0700

    mul promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log('Body:', user.body);
