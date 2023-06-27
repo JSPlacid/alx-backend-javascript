e03c8a2bfac6c6cc2f66dae0f28be2705231b12b
JSPlacid <ojoolusegun17@gmail.com>
2023-06-27 07:55:05 -0700

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,9 +2,7 @@ export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const response = 'This is the response from the API';
-
       resolve(response);
-
       // Simulate error by rejecting the Promise
       // reject(new Error('Failed to fetch data from the API'));
     }, 2000); // Delay of 2000 milliseconds (2 seconds)
