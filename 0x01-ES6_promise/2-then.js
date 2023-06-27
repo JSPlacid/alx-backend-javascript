e27deb62d2792629ebb4aff501c9539ab6f551c3
JSPlacid <ojoolusegun17@gmail.com>
2023-06-27 09:22:37 -0700

    catch me if u can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
     return promise
     .then(response => {
-	console.log('got a response from the API');
+	console.log('Got a response from the API');
 	return {
 	    status: 200,
 	    body: 'success'
