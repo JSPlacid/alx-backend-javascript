c07fdbcab6998b36022d958043b5ce3483def593
JSPlacid <ojoolusegun17@gmail.com>
2023-06-27 07:43:28 -0700

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,6 @@
-function getResponseFromAPI(){
-	promise = new promise(resolve, reject){
-	return promise
-	}
+export default function getResponseFromAPI(){
+	return new promise(() => {
+	cont res = 'this is a promise';
+	resolve(res)
+	});
 }
