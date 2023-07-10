668a670d09039a33c1ecda76568cbeca20accfa4
unknown <jspacific777@gmail.com>
2023-07-10 17:31:42 +0100

    correction

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = typeof sqft === 'number' ? sqft : 0;
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    if(!(subclass.evacuationWarningMessage))
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
