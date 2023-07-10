9a2fe5b7037ef38a685f1e22b9233fd1d3ffd72b
unknown <jspacific777@gmail.com>
2023-07-10 17:45:44 +0100

    correction

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,3 +1,14 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const newList = [class1, class2, class3];
+  return (newList);
+}
+
+/*
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
@@ -5,3 +16,4 @@ export default function initializeRooms() {
 
   return sizes.map((size) => new ClassRoom(size));
 }
+*/
